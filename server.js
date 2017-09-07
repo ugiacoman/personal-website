0bcbf1d6e49958a15047b63935e2bea94ef63648
Ulises Giacoman <ulises.giacoman@gmail.com>
2017-09-07 08:25:36 -0400

    Add custom server with ssl force

---
+++ server.js
@@ -0,0 +1,47 @@
+const next = require('next')
+const Hapi = require('hapi')
+const Good = require('good')
+const { defaultHandlerWrapper } = require('./next-wrapper')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const server = new Hapi.Server()
+
+// add request logging (optional)
+const pluginOptions = [
+  {
+    register: Good,
+    options: {
+      reporters: {
+        console: [{
+          module: 'good-console'
+        }, 'stdout']
+      }
+    }
+  }
+]
+
+app.prepare()
+.then(() => {
+  server.connection({ port })
+  server.register(pluginOptions)
+  server.register({
+    register: require('hapi-require-https'),
+    options: {}
+  })
+  .then(() => {
+    server.route({
+      method: 'GET',
+      path: '/{p*}', /* catch all route */
+      handler: defaultHandlerWrapper(app)
+    })
+
+    server.start().catch(error => {
+      console.log('Error starting server')
+      console.log(error)
+    }).then(() => {
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
+})
