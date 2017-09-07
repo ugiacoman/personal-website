06232d9ebb2900519f66dc267cefb7bcae437a61
Ulises Giacoman <ulises.giacoman@gmail.com>
2017-09-07 00:23:00 -0400

    Add resume

--- components/bio.js
+++ components/bio.js
@@ -5,7 +5,7 @@ export default () => (
       <strong>Ulises</strong> likes to build in the land of dev. Follow on <a id='instagram' href='//www.instagram.com/ulisantosg/' target='_blank'><TiSocialInstagram /></a>
     </p>
     <p>
-      <a id='github' href='//udiscover.me/resume.pdf' target='_blank'>Resume</a> | <a id='projects' href='//devpost.com/ugiacoman' target='_blank'>Projects</a> | <a id='github' href='//github.com/ugiacoman' target='_blank'>Github</a>
+      <a id='github' href='./static/resume.pdf' target='_blank'>Resume</a> | <a id='projects' href='//devpost.com/ugiacoman' target='_blank'>Projects</a> | <a id='github' href='//github.com/ugiacoman' target='_blank'>Github</a>
     </p>
   </div>
 )
